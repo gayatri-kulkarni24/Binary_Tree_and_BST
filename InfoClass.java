19d9b587a08c68820bed070548fdfa4b161459c0
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-05-09 20:15:13 +0530

    Create InfoClass.java

---
+++ InfoClass.java
@@ -0,0 +1,16 @@
+// static class Info{
+    //     int diam;
+    //     int ht;
+    //     public Info(int diam,int ht){
+    //         this.diam=diam;
+    //         this.ht=ht;
+    //     }
+    // }
+    static class Info{
+        Node node;
+        int hd; //horizontal distance from 0
+        public Info(Node node,int hd){
+            this.node=node;
+            this.hd=hd;
+        }
+    }
