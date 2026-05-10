2f8b0064a472ccd3426abfb63d39544adad0ce42
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-05-10 21:30:55 +0530

    Create inorder.java

---
+++ inorder.java
@@ -0,0 +1,8 @@
+public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
