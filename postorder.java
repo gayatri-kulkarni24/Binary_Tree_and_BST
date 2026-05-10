ff104469335b7037bc748cea65a8f5a480ab5d53
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-05-10 21:31:23 +0530

    Create postorder.java

---
+++ postorder.java
@@ -0,0 +1,8 @@
+public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+         System.out.print(root.data + " ");
+    }
