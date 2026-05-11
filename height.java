e5395e2e15344433f04d926372d6cc1a84082144
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-05-11 20:50:58 +0530

    Create height.java

---
+++ height.java
@@ -0,0 +1,8 @@
+public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return Math.max(lh,rh)+1;
+    }
