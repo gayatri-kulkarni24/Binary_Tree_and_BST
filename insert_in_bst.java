56b0aace1e4b79a437cf89ea88732f1352158010
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-08-01 20:12:25 +0530

    Create insert_in_bst.java

---
+++ insert_in_bst.java
@@ -0,0 +1,12 @@
+public static Node insert(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data > val){
+           root.left= insert(root.left,val);
+        }else{
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
