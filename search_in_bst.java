02557b73e95fd3dba4c39355358134b3221ab6db
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-08-02 22:00:44 +0530

    Create search_in_bst.java

---
+++ search_in_bst.java
@@ -0,0 +1,13 @@
+public static boolean search(Node root,int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data==key){
+            return true;
+        }
+        if(root.data > key){
+            return search(root.left,key);
+        }else{
+            return search(root.right,key);
+        }
+    }
