dc86d972a1ece457da4d17f65a316b0d722a4bcb
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-09-04 22:03:34 +0530

    Create preorder_in_bst.java

---
+++ preorder_in_bst.java
@@ -0,0 +1,9 @@
+public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data +" ");
+        preorder(root.left);
+        preorder(root.right);
+
+    }
